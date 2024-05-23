10f26ccf3b62a5af538b1df50c1867076448bf3d
ricardofigbr <62208904+ricardofigbr@users.noreply.github.com>
2024-05-23 04:07:07 -0300

    OrderItem, mano-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
--- src/main/java/com/educandoweb/course/resources/OrderResource.java
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -16,17 +16,17 @@ import com.educandoweb.course.services.OrderService;
 @RequestMapping(value = "/orders")
 public class OrderResource {
 
-	@Autowired
+	@Autowired 
 	private OrderService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll(){
-		List<Order> list = service.findAll();				
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
 		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
